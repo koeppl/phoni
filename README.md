ba44f40a221a609ef604c1fb6c7f95785b605d43
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2020-11-09 18:35:46 +0900

    update readme and benchmark desc

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
-# moni
-A Read Aligner with Multi-Genome References
+# PHONI 
+## - Practical Heuristic ON Incremental matching statistics computation
+
+This framework supports the currently memory-friendliest way to compute the matching statistics of a pattern on highly-repetitive texts,
+given that the input text is precomputed with the [https://github.com/maxrossi91/moni](MONI) index.
+
+We require the pattern and the text to be available in form of sequences stored in the `.fa` (FASTA) format.
+To use our solution, you need to have recent `cmake`, `g++`, `zsh`, and `python 3` installed.
+
+## Preparations
+
+We need the following python 3 packages for extracting and concatenating `.fa` files:
+```console
+	pip3 install biopython
+	pip3 install fastaparser
+	pip3 install psutil
+```
 
 
-pip3 install biopython
-pip3 install fastaparser
-pip3 install psutil
-ln -sv build/no_thresholds .
-wget 'http://dolomit.cs.tu-dortmund.de/chr19.10.fa.xz'; wget 'http://dolomit.cs.tu-dortmund.de/chr19.1000.fa.xz'
+```console
+git clone --branch phoni https://github.com/maxrossi91/moni
+```
 
+### Compile
 
-for phoni_0, you have to exchange lceToRBounded with lceToR_NaiveBounded in include/ms/phoni.hpp
+```console
+mkdir build
+cd build; cmake ..
+make
+```
+
+In our experiments we used the file
+[http://dolomit.cs.tu-dortmund.de/chr19.1000.fa.xz](chr19.1000.fa.xz)
+as our text dataset,
+and
+[http://dolomit.cs.tu-dortmund.de/chr19.10.fa.xz](chr19.10.fa.xz) 
+as our pattern dataset.
+We have a shell script `benchmark.sh` for an automatic benchmark.
+For this to work, some variables in it has to be set, as this project does not ship with the other matching statistic algorithms
+such as [https://github.com/odenas/indexed_ms](msfast) or [https://github.com/apachecom/rrepair](rrepair), 
+meaning it is necessary to download and compile those projects individually, and the set the corresponding variables in `benchmark.sh` manually.
+Finally, the output of `benchmark.sh` can be processed by [https://github.com/koeppl/sqlplot](sqlplots) to generate the plots shown in the paper.
+
+To compute the naive PHONI variant evaluated in the paper, simple exchange `lceToRBounded` with `lceToR_NaiveBounded` in the file `include/ms/phoni.hpp`.
+
--- benchmark.sh
+++ benchmark.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env zsh
-#
-
+# Evaluation script
+# Computes an output that is parsable with sqlplot (https://github.com/koeppl/sqlplot)
+# Either hardcode PATTERN_FILE for your `hostname` below, or run the script with the pattern filename as the first argument
 
 function die {
 	echo "$1" >&2
 	exit 1
 }
 
+# TODO: the text datasets we want to index: 
+datasets=(chr19.1.fa chr19.16.fa chr19.32.fa chr19.64.fa chr19.100.fa chr19.128.fa chr19.256.fa  chr19.512.fa chr19.1000.fa) 
 
+# TODO: add here your hostname and change the variables such that the programs for the evaluation can be found
 if [[ $(hostname) = "shannon" ]]; then
+
 PHONI_ROOTDIR=/home/dkoeppl/code/moni
 PHONI_BULIDDIR=$PHONI_ROOTDIR/build
 DATASET_DIR=/home/dkoeppl/data
-#INDEXEDMS_BUILDDIR=/s/nut/a/homes/dominik.koeppl/fast/code/indexed_ms/fast_ms/bin/
+INDEXEDMS_BUILDDIR=/s/nut/a/homes/dominik.koeppl/fast/code/indexed_ms/fast_ms/bin/
 RREPAIR_BULIDDIR=/home/dkoeppl/code/rrepair/build/
-
-
 PATTERN_FILE=$DATASET_DIR/chr19.10.fa
 LOG_DIR=$HOME/log/
 
 elif [[ $(hostname) = "elm" ]]; then
 
-PHONI_ROOTDIR=/s/nut/a/homes/dominik.koeppl/code/pfp/moni/
+PHONI_ROOTDIR=/s/nut/a/homes/dominik.koeppl/code/pfp/moni/ 
 PHONI_BULIDDIR=$PHONI_ROOTDIR/build
 DATASET_DIR='/s/nut/a/homes/dominik.koeppl/fast/data'
 INDEXEDMS_BUILDDIR=/s/nut/a/homes/dominik.koeppl/fast/code/indexed_ms/fast_ms/bin/
 RREPAIR_BULIDDIR=/s/nut/a/homes/dominik.koeppl/fast/code/rrepair/build/
-
-#ms_prg=$PHONI_BULIDDIR/test/src/ms
-#ms_prg=/s/nut/a/homes/dominik.koeppl/msrelease
-
 PATTERN_FILE=$DATASET_DIR/10_samples.fa
 LOG_DIR=$HOME/fast/log/
 
 else 
 	die "need setting for host $hostname"
 fi
+
+[[ -n $1 ]] && PATTERN_FILE="$1"
 
 base_prg=$PHONI_BULIDDIR/no_thresholds
 thresholds_prg=$PHONI_BULIDDIR/thresholds
@@ -59,11 +60,6 @@ phonibuild_prg=$PHONI_BULIDDIR/test/src/build_phoni
 rlbwt_prg=$PHONI_BULIDDIR/test/src/bwt2rlbwt
 
 
-datasets=(chr19.1.fa chr19.16.fa chr19.32.fa chr19.64.fa chr19.100.fa chr19.256.fa  chr19.512.fa chr19.1000.fa) #  chr19.100.fa  chr19.128.fa  chr19.16.fa  chr19.1.fa  chr19.256.fa  chr19.512.fa)
-#datasets=(chr19.64.fa) #chr19.16.fa chr19.100.fa chr19.128.fa chr19.256.fa  chr19.512.fa chr19.1000.fa) # chr19.256.fa  chr19.512.fa) #  chr19.100.fa  chr19.128.fa  chr19.16.fa  chr19.1.fa  chr19.256.fa  chr19.512.fa)
-#datasets=(chr19.1000.fa)
-
-datasets=(chr19.32.fa chr19.64.fa chr19.100.fa chr19.256.fa  chr19.512.fa chr19.1000.fa)
 
 alias Time='/usr/bin/time --format="Wall Time: %e\nMax Memory: %M"'
 
@@ -95,7 +91,7 @@ for filename in $datasets; do
 	test -e $dataset
 	rawdataset=$dataset.raw
 
-	basestats="RESULT file=${filename} "
+	basestats="RESULT pattern=${PATTERN_FILE} file=${filename} "
 
 	if [[ ! -e $dataset.bwt ]]; then 
 		stats="$basestats type=baseconstruction "
@@ -163,54 +159,53 @@ for filename in $datasets; do
  ## END MONI
  ###########
 
-continue
+	# for rrepair_round in $(seq 0 0); do
+	# 	basestats="$_basestats rrepair=$rrepair_round "
+	# 	if [[ $rrepair_round -eq 0 ]]; then
+	# 		logFile=$LOG_DIR/$filename.repair.${rrepair_round}.log
+	# 		stats="$basestats type=repair "
+    #
+	# 		set -x
+	# 		Time $bigrepair_prg $rawdataset > "$logFile" 2>&1
+	# 		set +x
+    #
+	# 		echo -n "$stats"
+	# 		echo -n "Csize=$(stat --format="%s" $rawdataset.C) Rsize=$(stat --format="%s" $rawdataset.R) "
+	# 		$readlog_prg $logFile
+    #
+	# 		logFile=$LOG_DIR/$filename.grammar.${rrepair_round}.log
+	# 		stats="$basestats type=grammar "
+	# 		set -x
+	# 		Time $slpenc_prg -e SelfShapedSlp_SdSd_Sd -f Bigrepair -i $rawdataset -o $dataset.slp > "$logFile" 2>&1
+	# 		set +x
+	# 		echo -n "$stats"
+	# 		echo -n "slpsize=$(stat --format="%s" $dataset.slp) "
+	# 		$readlog_prg $logFile
+	# 	else 
+	# 		logFile=$LOG_DIR/$filename.repair.${rrepair_round}.log
+	# 		stats="$basestats type=repair "
+    #
+	# 		cd $(dirname $rrepair_prg)
+	# 		set -x
+	# 		Time $rrepair_prg $rawdataset 10 100 $rrepair_round 100000000 > "$logFile" 2>&1
+	# 		set +x
+    #
+	# 		echo -n "$stats"
+	# 		echo -n "Csize=$(stat --format="%s" $rawdataset.C) Rsize=$(stat --format="%s" $rawdataset.R) "
+	# 		$readlog_prg $logFile
+    #
+	# 		logFile=$LOG_DIR/$filename.grammar.${rrepair_round}.log
+	# 		stats="$basestats type=grammar "
+	# 		set -x
+	# 		Time $slpenc_prg -e SelfShapedSlp_SdSd_Sd -f rrepair -i $rawdataset -o $dataset.slp > "$logFile" 2>&1
+	# 		set +x
+	# 		echo -n "$stats"
+	# 		echo -n "slpsize=$(stat --format="%s" $dataset.slp) "
+	# 		$readlog_prg $logFile
+	# 	fi
+	rrepair_round=0
 
-	for rrepair_round in $(seq 0 2); do
-		basestats="$_basestats rrepair=$rrepair_round "
-		if [[ $rrepair_round -eq 0 ]]; then
-			logFile=$LOG_DIR/$filename.repair.${rrepair_round}.log
-			stats="$basestats type=repair "
-
-			set -x
-			Time $bigrepair_prg $rawdataset > "$logFile" 2>&1
-			set +x
-
-			echo -n "$stats"
-			echo -n "Csize=$(stat --format="%s" $rawdataset.C) Rsize=$(stat --format="%s" $rawdataset.R) "
-			$readlog_prg $logFile
-
-			logFile=$LOG_DIR/$filename.grammar.${rrepair_round}.log
-			stats="$basestats type=grammar "
-			set -x
-			Time $slpenc_prg -e SelfShapedSlp_SdSd_Sd -f Bigrepair -i $rawdataset -o $dataset.slp > "$logFile" 2>&1
-			set +x
-			echo -n "$stats"
-			echo -n "slpsize=$(stat --format="%s" $dataset.slp) "
-			$readlog_prg $logFile
-		else 
-			logFile=$LOG_DIR/$filename.repair.${rrepair_round}.log
-			stats="$basestats type=repair "
-
-			cd $(dirname $rrepair_prg)
-			set -x
-			Time $rrepair_prg $rawdataset 10 100 $rrepair_round 100000000 > "$logFile" 2>&1
-			set +x
-
-			echo -n "$stats"
-			echo -n "Csize=$(stat --format="%s" $rawdataset.C) Rsize=$(stat --format="%s" $rawdataset.R) "
-			$readlog_prg $logFile
-
-			logFile=$LOG_DIR/$filename.grammar.${rrepair_round}.log
-			stats="$basestats type=grammar "
-			set -x
-			Time $slpenc_prg -e SelfShapedSlp_SdSd_Sd -f rrepair -i $rawdataset -o $dataset.slp > "$logFile" 2>&1
-			set +x
-			echo -n "$stats"
-			echo -n "slpsize=$(stat --format="%s" $dataset.slp) "
-			$readlog_prg $logFile
-		fi
-
-
+ if [[ ! -e $dataset.phoni ]]; then
 		logFile=$LOG_DIR/$filename.phonibuild.${rrepair_round}.log
 		stats="$basestats type=phonibuild "
 		set -x
@@ -219,7 +214,7 @@ continue
 		echo -n "$stats"
 		echo -n "phonisize=$(stat --format="%s" $dataset.phoni) "
 		$readlog_prg $logFile
-
+ fi
 
 		logFile=$LOG_DIR/$filename.phoni.${rrepair_round}.log
 		stats="$basestats type=phoni "
@@ -234,50 +229,50 @@ continue
 			echo -n "$basestats type=mscheck "
 			echo "check=\"$(diff -q $LOG_DIR/$filename.phoni.${rrepair_round}.lengths $LOG_DIR/$filename.phoni.0.lengths)\""
 		fi
-	done
+	# done
 
 
 done
 
 
 
-# ##################
-# ## START indexedms
-# ##################
-#for filename in $datasets; do
-#	dataset=$DATASET_DIR/$filename
-#	test -e $dataset
-#
-#	basestats="RESULT file=${filename} "
-#
-#	if [[ ! -r $dataset.raw.fwd.stree ]]; then
-#
-#		logFile=$LOG_DIR/$filename.indexms.const.log
-#		stats="$basestats type=indexedconst "
-#
-#		set -x
-#		Time $indexedms_index_prg -s_path $dataset.raw > "$logFile" 2>&1
-#		set +x
-#
-#		echo -n "$stats"
-#		echo -n "fwdsize=$(stat --format="%s" $dataset.raw.fwd.stree) revsize=$(stat --format="%s" $dataset.raw.rev.stree) "
-#		$readlog_prg $logFile
-#	fi
-# 
-# for patternseq in $pattern_dir/[0-9]; do
-#	patternnumber=$(basename $patternseq)
-#	logFile=$LOG_DIR/$filename.indexms.query.${patternnumber}log
-#	stats="$basestats type=indexedquery pattern=${patternnumber} "
-# 
-#	set -x
-#	Time $indexedms_query_prg -s_path $dataset.raw -t_path $patternseq -load_cst 1 > "$logFile" 2>&1
-#	set +x
-# 
-#	echo -n "$stats"
-#	$readlog_prg $logFile
-# done
-#
-#done
+##################
+## START indexedms
+##################
+basestats="$_basestats"
+for filename in $datasets; do
+	dataset=$DATASET_DIR/$filename
+	test -e $dataset
+
+
+	if [[ ! -r $dataset.raw.fwd.stree ]]; then
+
+		logFile=$LOG_DIR/$filename.indexms.const.log
+		stats="$basestats type=indexedconst "
+
+		set -x
+		Time $indexedms_index_prg -s_path $dataset.raw > "$logFile" 2>&1
+		set +x
+
+		echo -n "$stats"
+		echo -n "fwdsize=$(stat --format="%s" $dataset.raw.fwd.stree) revsize=$(stat --format="%s" $dataset.raw.rev.stree) "
+		$readlog_prg $logFile
+	fi
+
+for patternseq in $pattern_dir/[0-9]; do
+	patternnumber=$(basename $patternseq)
+	logFile=$LOG_DIR/$filename.indexms.query.${patternnumber}log
+	stats="$basestats type=indexedquery pattern=${patternnumber} "
+
+	set -x
+	Time $indexedms_query_prg -s_path $dataset.raw -t_path $patternseq -load_cst 1 > "$logFile" 2>&1
+	set +x
+
+	echo -n "$stats"
+	$readlog_prg $logFile
+done
+
+done
  ##################
  ## END indexedms
  ##################
